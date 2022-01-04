ab5c8b196cbd15a8d405dfef4ae4fb25d68f6e54
= <stelmaszv@gmail.com>
2022-01-04 13:11:24 +0100

    Add class "APIPrototype" and "SeadStartView" (view.py)

--- core/wideocollectorseader/urls.py
+++ core/wideocollectorseader/urls.py
@@ -1,4 +1,7 @@
-app_name = 'wideocollectorseader'
+from core.wideocollectorseader.views import SeadStartView
 
+app_name = 'wideocollectorseader'
+from django.urls import path
 urlpatterns = [
+    path('carts',                    SeadStartView.as_view(), name='sead_start'),
 ]
