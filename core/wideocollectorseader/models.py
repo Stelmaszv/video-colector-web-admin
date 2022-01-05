a3d799898189739d9570581f92d98b4328ffe67a
= <stelmaszv@gmail.com>
2022-01-05 17:09:45 +0100

    Add Model Movies

--- core/wideocollectorseader/models.py
+++ core/wideocollectorseader/models.py
@@ -50,6 +50,7 @@ class Star(models.Model):
     likes = models.IntegerField(default=0)
     avatar = models.CharField(max_length=200, default='', null=True)
     favourite = models.BooleanField(default=False)
+    show_name = models.CharField(max_length=200, default='', null=True)
     description = models.TextField(default='', null=True)
     weight      = models.IntegerField(default=0)
     height     = models.IntegerField(default=0)
@@ -61,6 +62,28 @@ class Star(models.Model):
     series = models.ManyToManyField(to='wideocollectorseader.Serie', related_name='StarSerie', blank=True)
     tags = models.ManyToManyField(to='wideocollectorseader.Tag', related_name='Starstags', blank=True)
     date_of_birth = models.DateField(null=True,blank=True)
+    added               = models.DateTimeField(auto_now=True)
+    rating              = models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    name = models.CharField(max_length=200)
+    show_name = models.CharField(max_length=200, default='', null=True)
+    view = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    avatar = models.CharField(max_length=200, default='', null=True)
+    favourite = models.BooleanField(default=False)
+    poster = models.CharField(max_length=200, default='', null=True)
+    description = models.TextField(default='', null=True)
+    country = models.CharField(max_length=200, default='', null=True)
+    date_relesed = models.DateField(null=True,blank=True)
+    dir = models.CharField(max_length=200, default='', null=True)
+    added               = models.DateTimeField(auto_now=True)
+    rating              = models.IntegerField(default=0)
+    stars = models.ManyToManyField(to='wideocollectorseader.Star', related_name='MovieStars', blank=True)
+    tags = models.ManyToManyField(to='wideocollectorseader.Tag', related_name='Moviestags', blank=True)
+    series = models.ManyToManyField(to='wideocollectorseader.Serie', related_name='MovieSerie', blank=True)
     def __str__(self):
         return self.name
 
