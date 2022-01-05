3dcbafe61aae49b44aaceb40a3aa50c05a9d61ef
= <stelmaszv@gmail.com>
2022-01-05 12:59:50 +0100

    Add opservers

--- core/wideocollectorseader/views.py
+++ core/wideocollectorseader/views.py
@@ -6,31 +6,42 @@ from rest_framework.response import Response
 from rest_framework.views import  APIView
 from .models import Producents
 
-class APIPrototype(APIView,ABC):
+class StartSeederView(APIView):
 
-    file_name=''
-    Model=None
+    def opserver(self,opservers):
+        for opserver in opservers:
+            opserver.Seed()
+
+    def api_get(self, request, *args, **kwargs):
+        opservers=[
+            ProducentSeader()
+        ]
+        self.opserver(opservers)
+        return Response(data=[], status=status.HTTP_200_OK)
+    def get(self, request, *args, **kwargs):
+        return self.api_get(request)
+
+class ApstractSeader(ABC):
+
+    file_name = ''
+    Model = None
+
+    def Seed(self):
+        if Path('./jsondb/' + self.file_name).is_file():
+            with open('./jsondb/' + self.file_name) as json_file:
+                data = json.load(json_file)
+                for item in data:
+                    if len(self.Model.objects.filter(name=item['name'])) == 0:
+                        self.add_model(item)
+        else:
+            print('Not found ' + self.file_name)
 
     @abstractmethod
     def add_model(self,item):
         pass
 
-    def api_get(self, request, *args, **kwargs):
-        if Path('./jsondb/'+self.file_name).is_file():
-            with open('./jsondb/'+self.file_name) as json_file:
-                data = json.load(json_file)
-                for item in data:
-                    if len(self.Model.objects.filter(name=item['name']))==0:
-                        self.add_model(item)
-            return Response(data=['New Data added'], status=status.HTTP_200_OK)
-        else:
-            print('Not found '+self.file_name)
-            return Response(data=['Not found '+self.file_name+''], status=status.HTTP_404_NOT_FOUND)
+class ProducentSeader(ApstractSeader):
 
-    def get(self, request, *args, **kwargs):
-        return self.api_get(request)
-
-class StartSeederView(APIPrototype):
     file_name = 'Producent.json'
     Model=Producents
 
@@ -44,3 +55,4 @@ class StartSeederView(APIPrototype):
             country=item['country'],
             description=item['description']
         ).save()
+
