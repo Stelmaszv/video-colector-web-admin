0f14207c9cb729b3dcaee87f9450b227ee2dc97b
= <stelmaszv@gmail.com>
2022-01-25 15:46:16 +0100

    Add stars in movies

--- front-end-src/src/app/movies/movies.component.ts
+++ front-end-src/src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
+import { StarsComponent } from '../stars/stars.component';
 
 
 @Component({
@@ -13,9 +14,36 @@ export class MoviesComponent implements OnInit {
 
   constructor(private httpService: HttpService) { }
 
-  private set_stars(movie:any):[]{
-    console.log(movie.stars);
-    return movie
+  private set_more(movie:any){
+    if (movie.stars.length>2){
+      return movie.stars.length-2
+    }
+    return 0
+  }
+
+  private set_stars(movie:any){
+    let better_stars=[]
+    let resst=[]
+    let count=0
+
+    for (let star of movie.stars){
+      if (star.views.length>0){
+        better_stars.push(star)
+      }
+    }
+
+    for (let star of movie.stars){
+      if (star.views.length==0){
+        resst.push(star)
+        count++
+      }
+    }
+
+    if (!better_stars.length){
+      let reset1=resst[Math.floor(Math.random()* resst.length)]
+      return better_stars.concat(reset1);
+    }
+    return better_stars;
   }
 
   private set_results():void{
@@ -23,8 +51,8 @@ export class MoviesComponent implements OnInit {
     
     for (let movie of this.movies){
       movie['js_stars']=this.set_stars(movie)
+      movie['more']=this.set_more(movie)
     }
-    console.log(this.movies)
   }
 
   public ngOnInit(): void {
