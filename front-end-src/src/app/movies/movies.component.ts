1758f6ebbea06b72c767d43eb3a2c1fce221f3be
= <stelmaszv@gmail.com>
2022-01-25 14:43:09 +0100

    Fix flex error on front end

--- front-end-src/src/app/movies/movies.component.ts
+++ front-end-src/src/app/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 
+
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
@@ -12,13 +13,30 @@ export class MoviesComponent implements OnInit {
 
   constructor(private httpService: HttpService) { }
 
-  ngOnInit(): void {
+  private set_stars(movie:any):[]{
+    console.log(movie.stars);
+    return movie
+  }
+
+  private set_results():void{
+    this.movies=this.movies.results
+    
+    for (let movie of this.movies){
+      movie['js_stars']=this.set_stars(movie)
+    }
+    console.log(this.movies)
+  }
+
+  public ngOnInit(): void {
     this.httpService.get_url('http://127.0.0.1:8000/movies')
     
     
     .subscribe(
       (response) => {
-        console.log(this.movies=response)
+        if (response.hasOwnProperty('results')){
+          this.movies=response
+          this.set_results()
+        }
       }
     );
   }
