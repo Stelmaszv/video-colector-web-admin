cc487e5629ae79f37ed18632a61b8e5cc8562987
= <stelmaszv@gmail.com>
2022-01-28 15:41:14 +0100

    Add Selected tags and stars

--- front-end-src/src/app/base-list/base-list.component.ts
+++ front-end-src/src/app/base-list/base-list.component.ts
@@ -34,15 +34,68 @@ export class BaseListComponent implements OnInit {
   public data:any;
   public url=''
   public filter_url=""
+  public tags_form:any
+  public stars_form:any
+  public loading:any=true
+  public search:any
+  public tags:any
+  public stars:any
+  public series_select:any
+  private series_select_url = 'http://127.0.0.1:8000/series_select'
+  private tag_select_url = 'http://127.0.0.1:8000/tags'
+  private star_select_url = 'http://127.0.0.1:8000/stars_form'
   protected results : any;
   protected response : any;
   protected page=1;
-  loading:any=true
+  
 
-  constructor(protected httpService: HttpService) { }
+  public constructor(protected httpService: HttpService) { }
 
+  public add_star(add_star:number):void
+  {
+    this.array_filter(this.stars_form,add_star)
+  }
 
-  public ngOnInit(): void {
+  public add_tag(add_tag:number):void
+  {
+    this.array_filter(this.tags_form,add_tag)
+  }
+
+  public if_exist(id:number,array:any[]):boolean
+  { 
+    let stan=false
+    for (let el of array){
+      if (el==id){
+        stan= true
+      }
+    }
+    return stan
+  }
+
+  public load_items_for_form():void
+  {
+    this.httpService.get_url(this.tag_select_url).subscribe(
+      (response) => {
+          this.tags=response
+      }
+    );
+    this.httpService.get_url(this.star_select_url).subscribe(
+      (response) => {
+          this.stars=response
+      }
+    );
+  }
+
+  public serch():void
+  {
+    this.page=1;
+    this.data=[]
+    this.set_form()
+    this.load_data()
+  }
+
+  public ngOnInit(): void 
+  {
     this.data=[]
     this.load_data()
     this.onInit()
@@ -50,11 +103,76 @@ export class BaseListComponent implements OnInit {
   }
 
   public onInit():void
-  {
+  {}
 
+  private set_results():void{
+    for (let movie of this.response.results){
+      this.on_set_results(movie)
+      if (this.add_if_not_exist(movie)){
+        this.data.push(movie)
+      }
+    }
   }
 
-  protected on_set_results(movie:any):any{}
+  private set_next():number
+  {
+    if (this.response.next != null){
+      return this.page+1
+    }else{
+      return this.page
+    }
+  }
+
+  private set_form():void
+  {
+    let form_elments = Object.keys(this.search.value);
+    this.filter_url=''
+    for (let item of form_elments){
+      if (this.search.value[item]!=null){
+        let string =item+'='+this.search.value[item]
+        this.filter_url+=string+'&'
+      }
+
+    }
+
+    for (let star of this.stars_form){
+      let string ='stars='+star
+      this.filter_url+=string+'&'
+    }
+
+    for (let tag of this.tags_form){
+      let string ='tags='+tag
+      this.filter_url+=string+'&'
+    }
+  }
+
+  private array_filter(array:any[],add:number):void
+  {
+    let stan=false
+    for (let el of array){
+      if (el==add){
+        stan= true
+      }
+    }
+    
+    if (stan){
+      array.splice(add)
+    }else{
+      array.push(add)
+    }
+  }
+
+  protected load_select():void
+  {
+    this.httpService.get_url(this.series_select_url).subscribe(
+      (response) => {
+          this.series_select=response
+      }
+    );
+  }
+
+  protected on_set_results(movie:any):void
+  {}
 
   protected load_data():void
   {
@@ -73,7 +191,8 @@ export class BaseListComponent implements OnInit {
     }
   }
 
-  protected add_if_not_exist(data:any):boolean{
+  protected add_if_not_exist(data:any):boolean
+  {
     let stan=true
     for (let item of this.data){
         if (item.id==data.id){
@@ -81,23 +200,6 @@ export class BaseListComponent implements OnInit {
         }
     }
     return stan
-  }
-
-  private set_results():void{
-    for (let movie of this.response.results){
-      this.on_set_results(movie)
-      if (this.add_if_not_exist(movie)){
-        this.data.push(movie)
-      }
-    }
-  }
-
-  private set_next():number{
-    if (this.response.next != null){
-      return this.page+1
-    }else{
-      return this.page
-    }
   }
 
   protected scroller():void
--- front-end-src/src/app/movies/movies.component.ts
+++ front-end-src/src/app/movies/movies.component.ts
@@ -8,123 +8,30 @@ import { FormControl ,FormGroup} from '@angular/forms';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent extends BaseListComponent{
-  mode ='cover'
-  series_select:any
-  tags:any
-  stars:any
-  tags_form:any
-  stars_form:any
+  public mode ='cover'
   public override url='http://127.0.0.1:8000/movies?page='
-  series_select_url = 'http://127.0.0.1:8000/series_select'
-  tag_select_url = 'http://127.0.0.1:8000/tags'
-  star_select_url = 'http://127.0.0.1:8000/stars_form'
 
-
-  search = new FormGroup({
+  override search = new FormGroup({
     name: new FormControl(),
-    series: new FormControl(),
+    serie: new FormControl(),
     likes_count: new FormControl(),
     avg_rating:  new FormControl(),
     ratings_count:new FormControl(),
     disLikes_count:new FormControl()
   });
 
-  override onInit(){
+  public override onInit(){
       this.tags_form=[]
       this.stars_form=[]
       this.load_select()
   }
 
-  add_if_not_exist_in_array(addtag:any):boolean{
-      for (let tag of this.tags_form){
-        if (tag==addtag){
-          return true
-        }
-      }
-      return false
-  }
-  private array_filter(array:any[],add:number){
-    let stan=false
-    for (let el of array){
-      if (el==add){
-        stan= true
-      }
-    }
-    
-    if (stan){
-      array.splice(add)
-    }else{
-      array.push(add)
-    }
-  }
-
-  add_star(add_star:number){
-    this.array_filter(this.stars_form,add_star)
-  }
-
-  add_tag(add_tag:number){
-    this.array_filter(this.tags_form,add_tag)
-  }
-
-  load_select(){
-    this.httpService.get_url(this.series_select_url).subscribe(
-      (response) => {
-          this.series_select=response
-      }
-    );
-  }
-
-  load_items_for_form():void
-  {
-    this.httpService.get_url(this.tag_select_url).subscribe(
-      (response) => {
-          this.tags=response
-      }
-    );
-    this.httpService.get_url(this.star_select_url).subscribe(
-      (response) => {
-          this.stars=response
-      }
-    );
-  }
-
-  change_mode(){
+  public change_mode(){
     if (this.mode=='cover'){
       this.mode='poster'
     }else if(this.mode=='poster'){
       this.mode='cover'
     }
-  }
-
-  set_form(){
-    let form_elments = Object.keys(this.search.value);
-    let count=0
-    this.filter_url=''
-    for (let item of form_elments){
-      if (this.search.value[item]!=null){
-        let string =item+'='+this.search.value[item]
-        this.filter_url+=string+'&'
-      }
-      
-    }
-
-    for (let star of this.stars_form){
-      let string ='stars='+star
-      this.filter_url+=string+'&'
-    }
-
-    for (let tag of this.tags_form){
-      let string ='tags='+tag
-      this.filter_url+=string+'&'
-    }
-
-  }
-
-  serch(){
-    this.page=1;
-    this.data=[]
-    this.set_form()
-    this.load_data()
   }
 
   private set_more(movie:any){
