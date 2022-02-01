e70cff6e1736a71f3ce9819ef3458253a812aa3a
= <stelmaszv@gmail.com>
2022-02-01 15:18:29 +0100

    Add buttuns for action

--- front-end-src/src/app/movieid/movieid.component.ts
+++ front-end-src/src/app/movieid/movieid.component.ts
@@ -13,7 +13,9 @@ export class MovieidComponent implements OnInit{
   protected url='http://127.0.0.1:8000/movie/';
   public data:any
   public mode='poster'
-  ngAfterViewInit:any
+  public if_favorite=false
+  public if_liked=false
+  public if_disliked=false
 
   constructor(private activatedRoute: ActivatedRoute,protected httpService: HttpService) { }
 
@@ -21,6 +23,21 @@ export class MovieidComponent implements OnInit{
   {
     this.set_id()
     this.get_url(this.id)
+  }
+
+  public add_like():void
+  {
+    this.if_liked=!this.if_liked
+  }
+
+  public add_dislike():void
+  {
+    this.if_disliked=!this.if_disliked
+  }
+
+  public add_to_if_favorite():void
+  {
+    this.if_favorite=!this.if_favorite
   }
 
   private set_id(): void  
