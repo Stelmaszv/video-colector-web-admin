465ad082987ce08a56699cbd591aa04ba484bed2
= <stelmaszv@gmail.com>
2022-02-02 16:56:04 +0100

    Add SeriesComponent Base

--- front-end-src/src/app/Components/id/serie-id/serie-id.component.ts
+++ front-end-src/src/app/Components/id/serie-id/serie-id.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import {BaseIDComponent} from '../base-id/base-id.component'
+BaseIDComponent
 @Component({
   selector: 'app-serie-id',
   templateUrl: './serie-id.component.html',
   styleUrls: ['./serie-id.component.scss']
 })
-export class SerieIdComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SerieIdComponent extends BaseIDComponent {
+  override url='http://127.0.0.1:8000/serie/'
 }
