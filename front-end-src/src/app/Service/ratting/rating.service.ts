0a4e5c9d8028bd67be3d629145cfdc40b173409f
= <stelmaszv@gmail.com>
2022-02-02 13:21:22 +0100

    Add Service RatingService

---
+++ front-end-src/src/app/Service/ratting/rating.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RatingService {
+
+  private rating(rate:any,ifs:boolean){
+    if (0 < parseInt(rate)){
+      if (rate > 3){
+        return true
+      }
+      return false
+    }else{
+      return false
+    }
+  }
+
+  public if_good_rating(movie:any){
+    let rate = parseInt(movie.avg_rating)
+    return this.rating(rate,rate > 3)
+  }
+
+  public if_mid_rating(movie:any){
+    let rate = parseInt(movie.avg_rating)
+    return this.rating(rate,rate == 3)
+  }
+
+  public if_low_rating(movie:any){
+    let rate = parseInt(movie.avg_rating)
+    return this.rating(rate,rate > 3)
+  }
+}
