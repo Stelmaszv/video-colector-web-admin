7e001180c8136457571f4163cd2db3c4ebac81a7
= <stelmaszv@gmail.com>
2022-02-03 13:48:44 +0100

    Add Star Component Table_information

--- front-end-src/src/app/Components/id/star/star.component.ts
+++ front-end-src/src/app/Components/id/star/star.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {BaseIDComponent} from '../base-id/base-id.component'
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.scss']
 })
-export class StarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class StarComponent extends BaseIDComponent {
+  override url='http://127.0.0.1:8000/star/'
 }
