a4cd65f7b9eb8433d759e5e40127531c9f4a4f7d
= <stelmaszv@gmail.com>
2022-02-28 14:30:26 +0100

    Add component "ApstractAdminComponent"

--- front-end-src/src/app/Components/admin/apstract-admin/apstract-admin.component.ts
+++ front-end-src/src/app/Components/admin/apstract-admin/apstract-admin.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseListComponent } from '../../list/base-list/base-list.component';
 
 @Component({
   selector: 'app-apstract-admin',
   templateUrl: './apstract-admin.component.html',
   styleUrls: ['./apstract-admin.component.scss']
 })
-export class ApstractAdminComponent implements OnInit {
+export class ApstractAdminComponent extends BaseListComponent  {
 
-  constructor() { }
+  protected item_url=''
+  protected edit_url=''
+  protected galery_url=''
+  protected tag_url=''
+  protected stats_url=''
 
-  ngOnInit(): void {
+  protected override on_set_results(movie:any):void
+  {
+    movie['item_url']=[this.item_url+'/',movie.id]
+    movie['edit_url']=[this.edit_url+'/',movie.id]
+    movie['galery_url']=[this.galery_url+'/',movie.id]
+    movie['tag_url']=[this.tag_url+'/',movie.id]
+    movie['stats_url']=[this.stats_url+'/',movie.id]
   }
-
 }
