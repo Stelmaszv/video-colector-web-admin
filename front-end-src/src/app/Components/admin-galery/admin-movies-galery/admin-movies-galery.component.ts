d59bfc524ae8f7476ee828f2850221a8beaed482
= <stelmaszv@gmail.com>
2022-03-03 16:28:20 +0100

    Add method "generate_photos" in component "AdminMoviesGaleryComponent"

--- front-end-src/src/app/Components/admin-galery/admin-movies-galery/admin-movies-galery.component.ts
+++ front-end-src/src/app/Components/admin-galery/admin-movies-galery/admin-movies-galery.component.ts
@@ -11,6 +11,7 @@ export class AdminMoviesGaleryComponent extends BaseIDComponent {
   public override url = 'http://127.0.0.1:8000/movie/' 
   public galery_url='http://127.0.0.1:8000/moviephotosview/'
   public edit_url='http://127.0.0.1:8000/movieupdata/'
+  public generate_url="http://127.0.0.1:8000/admin/galery/generate/moviecap/"
 
   photos = new FormGroup({
     number: new FormControl(0),
@@ -19,6 +20,10 @@ export class AdminMoviesGaleryComponent extends BaseIDComponent {
   public generate_photos(){
     if (this.photos.value.number){
       console.log(this.photos.value.number)
+      this.httpService.get_url_auth(this.generate_url+''+this.id+'/?genrate='+this.photos.value.number).subscribe(respanse=>{
+        console.log(respanse)
+        window.location.reload()
+      })
     }
   }
 }
