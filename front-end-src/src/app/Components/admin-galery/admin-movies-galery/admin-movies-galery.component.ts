07a49be8d9a375d6bd1580ae57dea94581d865e8
= <stelmaszv@gmail.com>
2022-03-03 15:22:49 +0100

    Add method "generate_photos" in component "AdminMoviesGaleryComponent"

--- front-end-src/src/app/Components/admin-galery/admin-movies-galery/admin-movies-galery.component.ts
+++ front-end-src/src/app/Components/admin-galery/admin-movies-galery/admin-movies-galery.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { BaseIDComponent } from '../../id/base-id/base-id.component';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-movies-galery',
@@ -10,4 +11,12 @@ export class AdminMoviesGaleryComponent extends BaseIDComponent {
   public override url = 'http://127.0.0.1:8000/movie/' 
   public galery_url='http://127.0.0.1:8000/moviephotosview/'
   public edit_url='http://127.0.0.1:8000/movieupdata/'
+
+  photos = new FormGroup({
+    number: new FormControl(0),
+  });
+
+  public generate_photos(){
+    console.log(this.photos.value.number)
+  }
 }
