5cde6212bcbfd4a42d06943b8b08d4e3217863c7
= <stelmaszv@gmail.com>
2022-03-03 16:36:13 +0100

    Add Genereteing_stan

--- front-end-src/src/app/Components/admin-galery/admin-movies-galery/admin-movies-galery.component.ts
+++ front-end-src/src/app/Components/admin-galery/admin-movies-galery/admin-movies-galery.component.ts
@@ -12,6 +12,7 @@ export class AdminMoviesGaleryComponent extends BaseIDComponent {
   public galery_url='http://127.0.0.1:8000/moviephotosview/'
   public edit_url='http://127.0.0.1:8000/movieupdata/'
   public generate_url="http://127.0.0.1:8000/admin/galery/generate/moviecap/"
+  public generate_stan=false
 
   photos = new FormGroup({
     number: new FormControl(0),
@@ -19,6 +20,7 @@ export class AdminMoviesGaleryComponent extends BaseIDComponent {
 
   public generate_photos(){
     if (this.photos.value.number){
+      this.generate_stan=true
       console.log(this.photos.value.number)
       this.httpService.get_url_auth(this.generate_url+''+this.id+'/?genrate='+this.photos.value.number).subscribe(respanse=>{
         console.log(respanse)
