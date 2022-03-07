2f319cfa5250d1b15bcf70b7afe3a00c3dd07dd2
= <stelmaszv@gmail.com>
2022-03-07 13:44:55 +0100

    Add scroller.js

---
+++ front-end-src/src/assets/scroller.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", (event) => {
+    var limit = document.body.offsetHeight - window.innerHeight;
+    let scrol_pos=15/100*limit
+    if (window.scrollY>scrol_pos){
+        document.querySelector(".small_menu").style.display='block';
+    }else{
+        document.querySelector(".small_menu").style.display='none';
+    }
+})
