86ed6cec645184ac345c30f3c331b03ba04a2612
= <stelmaszv@gmail.com>
2022-03-16 16:30:30 +0100

    Add Alert to Actions

--- front-end-src/src/app/common/actions/actions.component.ts
+++ front-end-src/src/app/common/actions/actions.component.ts
@@ -3,6 +3,7 @@ import { HttpService } from 'src/app/Service/http/http.service';
 import { TokkenService } from 'src/app/Service/tokken/tokken.service';
 import { FormControl ,FormGroup} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertService } from 'src/app/Service/alert/alert.service';
 @Component({
   selector: 'app-actions',
   templateUrl: './actions.component.html',
@@ -18,11 +19,20 @@ export class ActionsComponent implements OnInit{
   @Input() edit_link:any
   private id=0
   public if_favorite=false
-  constructor(private httpService: HttpService,private TokkenService: TokkenService,private Router:Router,private ActivatedRoute: ActivatedRoute) { }
-
+  constructor(
+    private httpService: HttpService,
+    private TokkenService: TokkenService,
+    private Router:Router,
+    private ActivatedRoute: ActivatedRoute,
+    private AlertService:AlertService) 
+    {}
+      
+  
   public add_to_if_favorite(){
     this.add_action(this.add_to_favorite_url)
     this.if_favorite=!this.if_favorite
+    var stan = (this.if_favorite) ? 'added to' :'removed form';
+    this.AlertService.alert('Object '+stan+' favorite')
   }
 
 
@@ -36,14 +46,17 @@ export class ActionsComponent implements OnInit{
   }
 
   public add_like(){
+    this.AlertService.alert('Like Added')
     this.add_action(this.add_like_url,'.like_js')
   }
 
   public add_to_rating(){
+    this.AlertService.alert('Rate Added')
     this.add_action(this.add_to_rating_url+'?rate='+this.addrate.value.rate,'.ratting_js')
   }
 
   public add_dislike(){
+    this.AlertService.alert('Dislike Added')
     this.add_action(this.add_to_dislike_url,'.dislike_js')
   }
 
@@ -53,7 +66,6 @@ export class ActionsComponent implements OnInit{
 
   public edit(){
     this.set_id()
-    console.log(this.edit_link)
     this.Router.navigate([this.edit_link])
   }
 
