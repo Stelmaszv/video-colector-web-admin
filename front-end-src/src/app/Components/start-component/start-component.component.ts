eb129da1284414690c184b1d5873be29cb91c0d9
= <stelmaszv@gmail.com>
2022-03-16 13:46:12 +0100

    Add order By in component top

--- front-end-src/src/app/Components/start-component/start-component.component.ts
+++ front-end-src/src/app/Components/start-component/start-component.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponentComponent implements OnInit {
 
-  public movies_url:string='http://127.0.0.1:8000/top/movies?order=-views_count'
+  public movies_url:string='http://127.0.0.1:8000/top/movies?order='
+  public movies_order='-views_count'
 
   ngOnInit(): void {
     console.log('wqdd')
--- front-end-src/src/app/Components/top-section/top-section.component.ts
+++ front-end-src/src/app/Components/top-section/top-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { BaseListComponent } from '../list/base-list/base-list.component';
 
 @Component({
@@ -10,5 +11,19 @@ import { BaseListComponent } from '../list/base-list/base-list.component';
 export class TopSectionComponent extends BaseListComponent {
   @Input() public section_title:String=''
   @Input() public override url:any
+  @Input() public order:any
+  public start_url:any
   protected override paginate=false
+  override debug =true
+  public order_form = new FormControl();
+  public override on_before_load_data(): void {
+    this.start_url=this.url
+    this.url=this.url+this.order  
+  } 
+
+  public set_order(){
+    this.data=[]
+    this.url=this.start_url+this.order_form.value
+    this.load_data()
+  }
 }
